f8052397f73b82c91e2f7c7c19e4a5f7dac679ba
RayDy143 <alfeche492@outlook.com>
2020-03-08 11:11:14 +0800

    Layout header and footer added

---
+++ application/views/layout/footer.php
@@ -0,0 +1,9 @@
+	<footer class="page-footer">
+		<div class="container">
+			<div class="footer-copyright">
+				<div class="container">© 2014 Copyright Text</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?php echo $title; ?></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/materialize.min.css">
+</head>
+<body>
+	<nav>
+		<div class="nav-wrapper">
+			<a href="#" class="brand-logo">YTKaraoke</a>
+			<ul id="nav-mobile" class="righ hide-on-med-and-down">
+				<li><a href="#">User</a></li>
+				<li><a href="#">Songs</a></li>
+				<li><a href="#">Logout</a></li>
+			</ul>
+		</div>
+	</nav>
